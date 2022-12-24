ed455eff4e3679d5c158226a7ec5f1ec2d929afe
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-12-24 09:18:14 +0530

    Add files via upload

---
+++ Input_and_Output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    
+    cin>>i>>j>>k;
+    
+    cout<<i+j+k;
+       
+    return 0;
+}
